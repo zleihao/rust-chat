23012c4e474ecb38575c76a371aba7a2a99936b2
hao <im_leihao@163.com>
2024-08-10 08:56:51 +0800

    增加保存用户信息数据结构

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,48 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{Write, Read};
+use std::io::{Read, Write};
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::collections::HashMap;
+use std::io;
+use std::sync::{LazyLock, Mutex};
 
+struct Info {
+    fd: TcpStream,
+    addr: io::Result<SocketAddr>,
+}
+
+//欢迎语
 const STR: &[u8; 23] = b"Welcome to rust chat...";
+//监听任意地址，端口号默认8080
 const IP_PORT: &str = "0.0.0.0:8080";
 
+//使用HashMap保存用户连接信息
+static mut CLIENT_INFO: LazyLock<Mutex<std::collections::HashMap<String, Info>>> = LazyLock::new(|| {
+    Mutex::new(HashMap::new())
+});
+
 fn handler(mut s: TcpStream) {
-    let mut buf = [0u8;1024];
+    let mut buf = [0u8; 1024];
+
+    unsafe {
+        let mut c = CLIENT_INFO.lock().unwrap();
+        c.insert("1".to_string(), Info { fd: s.try_clone().unwrap(), addr: s.peer_addr() });
+    }
 
     loop {
-        let ret = match s.read(&mut buf) {
-            Ok(n) => n,
-            Err(e) => {
-                println!("{e}");
-                0
-            }, 
-        };
+        let ret = s.read(&mut buf).unwrap_or_else(|e| {
+            println!("{e}");
+            0
+        });
 
         if ret <= 0 {
-            println!("{:?} disconnect", s.peer_addr());
+            unsafe {
+                let c = CLIENT_INFO.lock().unwrap();
+                println!("{:?} disconnect",  c.get("1").unwrap().addr);
+            }
             let _ = s.shutdown(std::net::Shutdown::Both);
             return;
         }
 
+        //解析指令
         let _ = s.write(&buf[0..ret]);
         let _ = std::io::stdout().write(&buf[0..ret]);
     }
@@ -40,7 +61,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     handler(s);
                 });
                 let _ = tid.join();
-            },
+            }
             Err(e) => println!("{e}"),
         }
     }
