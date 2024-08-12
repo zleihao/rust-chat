8ea4e95fd33f96b2b2f5f2c1cd5d453dc09c8db3
hao <im_leihao@163.com>
2024-08-12 20:49:14 +0800

    实现群聊功能

--- README.md
+++ README.md
@@ -1,1 +1,149 @@
-Rust 版 TCP 聊天室
+# Rust tcp聊天室
+
+> 该聊天室是一个命令交互式的，client只需实现读取用户键盘输入，接收服务器发送的数据即可。
+
+
+
+## 运行
+
+~~~c
+cargo run
+~~~
+
+
+
+## 支持的命令
+
+> 命令是以 # 开头
+
+1. **help**
+
+   ```c
+   #help
+   ```
+
+   ![](./image/2.png)
+
+2. **setname**
+
+   ~~~c
+   #setname <user_name>
+   ~~~
+
+   ![](./image/3.png)
+
+3. **login**
+
+   ~~~c
+   #login <user_name> <user_pass>
+   ~~~
+
+   ![](./image/4.png)
+
+4. **logout**
+
+   ~~~c
+   #logout
+   ~~~
+
+   ![image-20240728192824456](./image/5.png)
+
+5. **broadcast**
+
+   ~~~c
+   #broadcast <string>
+   or
+   #b <string>
+   ~~~
+
+   ![](./image/1.png)
+
+## 客户端 Demo
+
+客户端代码可以参考一下代码编写，不仅限于C语言。
+
+```c
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+
+#define MAXLINE 2048
+
+#define max(x, y) ((x) > (y) ? x : (y))
+
+void str_in(int fd)
+{
+   char tx_buf[MAXLINE], rx_buf[MAXLINE];
+   int maxfdpl, stdineof;
+   fd_set rset;
+   int n;
+
+   stdineof = 0;
+   FD_ZERO(&rset);
+
+   while (1) {
+      if (0 == stdineof) {
+     		FD_SET(fileno(stdin), &rset);
+        }
+        FD_SET(fd, &rset);
+      maxfdpl = max(fileno(stdin), fd) + 1;
+       select(maxfdpl, &rset, NULL, NULL, NULL);
+
+       if (FD_ISSET(fd, &rset)) {
+           if ((n = read(fd, rx_buf, MAXLINE)) == 0) {
+               if (1 == stdineof) {
+                   return;
+               } else {
+                   fprintf(stderr, "str_cli: server terminated prematurely");
+                   break;
+               }
+           }
+           write(fileno(stdout), rx_buf, n);
+       }
+
+       if (FD_ISSET(fileno(stdin), &rset)) {
+           if ((n = read(fileno(stdin), rx_buf, MAXLINE)) == 0) {
+               stdineof = 1;
+               shutdown(fd, SHUT_WR);
+               FD_CLR(fileno(stdin), &rset);
+               continue;
+           }
+           write(fd, rx_buf, n);
+       }
+   }
+
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, cfd;
+    struct sockaddr_in client_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        printf("socket error\n");
+        return 0;
+    }
+
+    bzero(&client_addr, sizeof(client_addr));
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(8080);
+    inet_pton(AF_INET, argv[1], &client_addr.sin_addr);
+
+    cfd = connect(sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr));
+    if (cfd < 0) {
+        printf("connect error\n");
+        return 0;
+    }
+
+    str_in(sockfd);
+    exit(0);
+}
+```
+
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::sync::{LazyLock, Mutex};
 struct Info {
     fd: TcpStream,
     addr: io::Result<SocketAddr>,
+    name: String,
     pass: String,
     state: bool, //是否在线
 }
@@ -22,9 +23,9 @@ static mut CLIENT_INFO: LazyLock<Mutex<std::collections::HashMap<String, Info>>>
     LazyLock::new(|| Mutex::new(HashMap::new()));
 
 //解析用户命令
-fn parse(mut fd: TcpStream, data: &str) -> anyhow::Result<(), String> {
+fn parse(mut fd: TcpStream, name: String, data: &str) -> anyhow::Result<String, String> {
     if 0 == data.len() {
-        return Err("all space".to_string());
+        return Err("\n".to_string());
     }
 
     //判断是否满足指令格式
@@ -41,27 +42,67 @@ fn parse(mut fd: TcpStream, data: &str) -> anyhow::Result<(), String> {
                 return Err("用法错误\n".to_string());
             }
             if let Err(e) = set_name(fd.try_clone().unwrap(), &parts) {
-                let _ = fd.write(e.as_bytes());
-            } else {
-                let _ = fd.write("昵称设置成功，默认密码为：123\n".as_bytes());
+                // let _ = fd.write(e.as_bytes());
+                return Err(e);
             }
+
+            let _ = fd.write("昵称设置成功，默认密码为：123\n".as_bytes());
             println!("cmd = {}, name = {}", cmd, parts[1]);
+            return Ok(parts[1].to_string());
         }
         "#login" => {
             if len != 2 {
                 return Err("用法错误\n".to_string());
             }
-            println!("该命令是登录");
+            if let Err(e) = login(fd.try_clone().unwrap(), &parts) {
+                return Err(e);
+            }
+            let _ = fd.write("登录成功\n".as_bytes());
+            return Ok(parts[1].to_string());
+        }
+        "#logout" => {
+            unsafe {
+                let mut mux = CLIENT_INFO.try_lock().unwrap();
+                if let Some(v) = mux.get_mut(&name) {
+                    if v.state {
+                        v.state = false;
+                        let _ = fd.write("注销成功\n".as_bytes());
+                    } else {
+                        return Err("你还未登录不需要注销\n".to_string());
+                    }
+                } else {
+                    return Err("你还未登录不需要注销\n".to_string());
+                }
+            }
+        }
+        "#broadcast" | "#b" => {
+            if len < 1 {
+                return Err("用法错误\n".to_string());
+            }
+
+            broadcast(fd.try_clone().unwrap(), name.clone(), &parts);
+        }
+        "#help" => {
+            if len > 0 {
+                return Err("用法错误\n".to_string());
+            }
+            let help_info = "目前聊天室支持以下指令：\n\
+                                        \r注册：\t    #setname <user name>\n\
+                                        \r登录：\t    #login <user name> <pass>\n\
+                                        \r注销：\t    #logout\n\
+                                        \r群聊：\t    #broadcast <string>\n\n".to_string();
+            let _ = fd.write_all(help_info.as_bytes());
         }
         _ => {
             return Err("未知命令\n".to_string());
         }
     };
-    Ok(())
+    Ok(String::new())
 }
 
 fn handler(mut s: TcpStream) {
     let mut buf = [0u8; 1024];
+    let mut name: String = String::new();
     loop {
         buf.fill(0);
         let ret = s.read(&mut buf).unwrap_or_else(|e| {
@@ -70,6 +111,15 @@ fn handler(mut s: TcpStream) {
         });
 
         if ret <= 0 {
+            unsafe {
+                let mut mux = CLIENT_INFO.lock().unwrap();
+                if let Some(v) = mux.get_mut(&name) {
+                    v.state = false;
+                    println!("用户 {} 已断开聊天室的网", v.name);
+                }
+            }
+
+            println!("disconnect from {:?}, {name}", s.peer_addr());
             let _ = s.shutdown(std::net::Shutdown::Both);
             return;
         }
@@ -77,15 +127,15 @@ fn handler(mut s: TcpStream) {
         let data = String::from_utf8_lossy(&mut buf[0..ret]).to_ascii_lowercase();
         let data = data.trim_ascii_start();
 
-        if let Err(e) = parse(s.try_clone().unwrap(), &data) {
-            match e.as_str() {
-                "all space" => continue,
-                _ => {
-                    println!("{e}");
-                    if let Err(_) = s.write(e.as_bytes()) {
-                        continue;
-                    }
-                },
+        match parse(s.try_clone().unwrap(), name.clone(), &data) {
+            Ok(ok) => {
+                if !ok.is_empty() {
+                    name = ok
+                }
+            },
+            Err(e) => {
+                let _ = s.write(e.as_bytes());
+                continue;
             }
         };
     }
@@ -99,11 +149,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             Ok(mut s) => {
                 println!("Connect from {}", s.peer_addr()?);
                 let _ = s.write(STR);
-
-                let tid = std::thread::spawn(|| {
+                //为每一个用户创建一个线程
+                std::thread::spawn(|| {
                     handler(s);
                 });
-                let _ = tid.join();
             }
             Err(e) => println!("{e}"),
         }
@@ -116,21 +165,64 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn set_name(fd: TcpStream, buf: &Vec<&str>) -> anyhow::Result<(), String> {
     //判断是否已经有该昵称
     unsafe {
-        let mut mux = CLIENT_INFO.try_lock().unwrap();
-        if let Some(_) = mux.get(&buf[0].to_string()) {
+        let mut mux = CLIENT_INFO.lock().unwrap();
+        if let Some(_) = mux.get(&buf[1].to_string()) {
             //该昵称已存在，直接返回错误
             return Err("该昵称已存在，请换个昵称试试\n".to_string());
         } else {
             //该昵称不存在
-            mux.insert(buf[0].to_string(), Info {
+            mux.insert(buf[1].to_string(), Info {
                 fd: fd.try_clone().unwrap(),
                 addr: fd.peer_addr(),
+                name: buf[1].to_string(),
                 pass: "123".to_string(),
                 state: false,
             });
-            println!("{:?}", mux.get(&buf[0].to_string()).unwrap().addr);
+            println!("{:?}", mux.get(&buf[1].to_string()).unwrap().addr);
         }
     }
 
     Ok(())
+}
+
+//登录
+fn login(fd: TcpStream, buf: &Vec<&str>) -> anyhow::Result<(), String> {
+    //判断是否已经有该昵称
+    unsafe {
+        let mut mux = CLIENT_INFO.lock().unwrap();
+        if let Some(v) = mux.get_mut(&buf[1].to_string()) {
+            if v.state {
+                return Err("该用户已经登录了....".to_string());
+            }
+            //该昵称已存在，直接登录
+            if v.pass != buf[2].to_string() {
+                return Err("密码不对，请重新尝试\n".to_string());
+            }
+
+            v.state = true;
+            v.fd = fd.try_clone().unwrap();
+            return Ok(());
+        };
+        //该昵称不存在，直接返回错误
+        return Err("该昵称不存在，请先注册账户后重试\n".to_string());
+    }
+}
+
+//广播
+fn broadcast(fd: TcpStream, name: String, buf: &Vec<&str>) {
+    unsafe {
+        let map = CLIENT_INFO.try_lock().unwrap();
+        for (_, v) in &*map {
+            if v.fd.peer_addr().unwrap() == fd.peer_addr().unwrap() {
+                continue;
+            }
+            let msg = format!("From User {}: {}\n", name, buf[1..].join(" "));
+            // 发送消息给所有在线的用户
+            if v.state {
+                if let Err(e) = v.fd.try_clone().unwrap().write_all(msg.as_bytes()) {
+                    eprintln!("发送消息给 {} 失败: {}", v.name, e);
+                }
+            }
+        }
+    }
 }
